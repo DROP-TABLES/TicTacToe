e18b6ba3c2d6e6e5fcefac5cc617e49eef91ce56
Dagur Arinbjörn Daníelsson <dagur13@i-ee251479.greenqloud.internal>
2014-10-28 15:20:16 +0000

    player base class with tests

---
+++ src/main/java/game/Player.java
@@ -0,0 +1,19 @@
+package game;
+
+public class Player{
+	
+	private String mark;
+	
+	public Player(String m){
+		this.mark = m;
+	}
+
+	public String GetMove(){
+		return null;
+	}
+
+	public String GetMark(){
+		return mark;
+	}
+
+}
