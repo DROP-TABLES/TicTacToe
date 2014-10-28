b932b382fc212683b9e5344d48cef13953e7bdae
Steinar Þór Árnason <steinar@i-ee251479.greenqloud.internal>
2014-10-28 19:12:23 +0000

    Added IsFull function to Board

--- src/main/java/game/Board.java
+++ src/main/java/game/Board.java
@@ -12,6 +12,15 @@ public class Board{
 		}
 	}
 
+	public boolean IsFull(){
+		for(int x = 0; x < 3; x++) {
+			for(int y = 0; y < 3; y++) {
+				if(CanPlaceMark(x, y)) return false;
+			}
+		}
+		return true;
+	}
+
 	public boolean CanPlaceMark(int x, int y){
 		return boxes[x][y] == " ";
 	}
